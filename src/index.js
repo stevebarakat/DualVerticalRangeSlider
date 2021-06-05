4538ce60777fbb597366ae5bb024ec9f3517e37b
Steve <stevebarakat@gmail.com>
2021-06-05 02:17:33 -0400

    ruff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RangeSlider from './RangeSlider';
+import DualRangeSlider from './DualRangeSlider';
 import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RangeSlider min={235} max={1090} decimals={0} step={0} width="500px" primaryColor="hsl(196, 100%, 48%)" />
+    hello
+    <DualRangeSlider min={235} max={1090} decimals={0} step={0} width="500px" primaryColor="hsl(196, 100%, 48%)" />
+    hello
   </React.StrictMode>,
   document.getElementById('root')
 );
